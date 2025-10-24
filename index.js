007fea196dc874642ac5dd0e387f4a3a614a4db3
alexchagas-acm <alexchagas@gmail.com>
2025-10-24 19:29:23 -0300

    atualização do projeto v1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-flsdfsdfsdfsdf
+let Heroi = "Chespirito"
+let xpHeroi = 5002
+let nivelHeroi
+
+if(xpHeroi <= 1000){
+    nivelHeroi = "FERRO"
+
+}else if(xpHeroi < 2001){
+    nivelHeroi = "BRONZE"
+
+}else if(xpHeroi < 5001){
+    nivelHeroi = "PRATA"
+}else if(xpHeroi < 7001){
+    nivelHeroi = "OURO"
+}else if(xpHeroi < 8001){
+    nivelHeroi = "PLATINA"
+}else if(xpHeroi < 9001){
+    nivelHeroi = "ASCENDENTE"
+}else if(xpHeroi < 10001){
+    nivelHeroi = "IMORTAL"
+}else{
+    nivelHeroi = "RADIANTE"
+}
+console.log("O herói " + Heroi + " possui o nível " + nivelHeroi)
